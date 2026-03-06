d14bc226084e71c51a06af2db3627dcd45a61b59
riazuddin4 <mdriaj2010bd@gmail.com>
2026-03-07 01:58:27 +0600

    java script for card display is finished

--- script/main.js
+++ script/main.js
@@ -4,13 +4,78 @@ const buttons = document.querySelectorAll(".filter-btn");
 let issues = [];
 
 async function loadIssues() {
-  const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
-  const data = await res.json();
+    const res = await fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues");
+    const data = await res.json();
 
-  issues = data.data;
+    issues = data.data;
 
-  renderIssues(issues);
+    renderIssues(issues);
 }
 
 loadIssues();
+
+function renderIssues(issueList) {
+    cardContainer.innerHTML = "";
+
+    issueList.forEach((issue) => {
+
+        const priorityColor =
+            issue.priority === "high"
+                ? "border-red-500"
+                : issue.priority === "medium"
+                    ? "border-yellow-500"
+                    : "border-purple-500";
+
+        const statusColor =
+            issue.status === "open" ? "border-green-500" : "border-purple-500";
+
+        // const labels = issue.labels
+        //   .map(
+        //     (label) =>
+        //       `<span class="badge badge-outline badge-warning">${label}</span>`
+        //   )
+        //   .join("");
+        const labels = issue.labels?.map(
+            (label) => `<span class="badge badge-outline badge-warning">${label}</span>`
+        ).join("") || "";
+
+        const card = `
+    <div class="card bg-white border-t-4 ${priorityColor} shadow-md p-4">
+
+    <div class="flex justify-between items-center mb-2">
+
+    <div class="w-5 h-5 flex items-center justify-center rounded-full border ${statusColor}">
+      <div class="w-2 h-2 ${issue.status === "open" ? "bg-green-500" : "bg-purple-500"
+            } rounded-full"></div>
+    </div>
+
+    <span class="text-xs px-3 py-1 rounded-full bg-red-100 text-red-500 font-semibold">
+      ${issue.priority}
+    </span>
+
+    </div>
+
+    <h3 class="font-semibold text-sm">${issue.title}</h3>
+
+    <p class="text-xs text-gray-500 mt-1">
+      ${issue.description}
+    </p>
+
+    <div class="flex gap-2 mt-3">
+      ${labels}
+    </div>
+
+    <div class="text-xs text-gray-400 mt-4">
+      #${issue.id} by ${issue.author} <br>
+      ${issue.createdAt}
+    </div>
+
+    </div>
+    `;
+
+        cardContainer.innerHTML += card;
+    });
+}
+
+// const buttons = document.querySelectorAll("button");
 
